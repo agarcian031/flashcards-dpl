c95d6494c8eb48cf3444f3882c977b7d6ac29fdf
Amanda Garcia <algarcian@outlook.com>
2019-05-31 19:21:34 -0600

    jquery funct for card flip on click

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-$(document).ready(function() {
-const flip_card = $(".flip-card"); 
-
-flip_card.click(() => {
-  
-}); 
+$(document).ready(function () {
+const add_card = $('#add-card'); 
 
 
+  $('#flipcard').click( () => {
+    $('#flipcard').toggleClass('flipped');
+  });
+
+
+  // Add a card 
+  add_card.click(() => {
+      $(".cards").append(`
+      <div class="card-container">
+      <div class="container">
+        <div id="flipcard">
+          <div class="front">
+            <h2>Question</h2>
+          </div>
+          <div class="back">
+            <p>Answer</p>
+          </div>
+        </div>
+        <div id="button-group">
+          <button id="edit-card" class="btn">Edit Card</button>
+          <button id="delete-card" class="btn">Delete Card</button>
+        </div>
+      </div>
+    </div>
+      `);
+    newCard(); 
+  }); 
+
+  // Fill in the data for new card 
+const newCard = () => {
+  alert("worked!")
+}
+
+
+  // Edit cards
+
+
+
+  //Delete a card 
 
 
 
 
-
-
-
-
-
-}); //end document ready 
+});
